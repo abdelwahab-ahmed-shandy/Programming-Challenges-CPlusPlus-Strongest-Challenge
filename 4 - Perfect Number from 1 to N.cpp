fa85a863b741ff3b6b6a2bef9ca87a199889e34c
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-10-21 21:45:06 +0300

    Problem / 4
    4 - Perfect Number from 1 to N

---
+++ 4 - Perfect Number from 1 to N.cpp
@@ -0,0 +1,46 @@
+// Perfect Number
+// perfect Number = all Sum(divisors)
+
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int CheckPositiveNumber(string Mseeage)
+{
+	int Number = 0;
+
+	do
+	{
+		cout << Mseeage;
+		cin >> Number;
+	} while (Number <= 0);
+
+	return Number;
+}
+
+bool IsPrimeNum(int Number)
+{
+	int Sum = 0;
+	for (int i = 1; i < Number; i++)
+	{
+		if (Number % i == 0)
+			Sum = Sum + i;
+	}
+	return Number == Sum;
+}
+
+void PrintCheckPrame(int Number)
+{
+	for (int i = 1 ; i < Number; i++)
+	{
+		if (IsPrimeNum(i))
+			cout << i << endl;
+	}
+}
+
+int main()
+{
+	PrintCheckPrame(CheckPositiveNumber("Enter Positive Number : "));
+	return 0;
+}
