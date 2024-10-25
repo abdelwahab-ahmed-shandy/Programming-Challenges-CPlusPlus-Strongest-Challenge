83b2efea0cee1450e8e20c9840b67b96f1e6e8f2
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-10-25 14:11:18 +0300

    Update 2 - 7 - All Words From AAA To ZZZ.cpp

--- 2 - 7 - All Words From AAA To ZZZ.cpp
+++ 2 - 7 - All Words From AAA To ZZZ.cpp
@@ -22,3 +22,38 @@ int main()
 	AllWords();
 	return 0;
 }
+
+//=============================================================================================================================================
+//=============================================================================================================================================
+// 16 - All Words From AAA To ZZZ
+
+#include <iostream>
+
+using namespace std;
+
+void PrintWordsFromAAAtoZZZ()
+{
+	string word = "";
+	for (int i = 65; i <= 90; i++)
+	{
+		for (int j = 65; j <= 90; j++)
+		{
+			for (int k = 65; k <= 90; k++)
+			{
+				word = word + char(i);
+				word = word + char(j);
+				word = word + char(k);
+				cout << word << endl;
+				word = "";
+			}
+		}
+	cout << "\n____________________________\n";
+	}
+}
+
+int main()
+{
+	PrintWordsFromAAAtoZZZ();
+	return 0;
+}
+
